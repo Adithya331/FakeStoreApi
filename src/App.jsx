a1a21a0f46b0c1bd6de50dfb87b6705db1c7d2d0
Adithya331 <velaparthiadithya@gmail.com>
2024-09-30 19:49:28 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import React, { useEffect, useState } from "react"
 import axios from 'axios'
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
+import Grid from '@mui/material/Grid';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-
 
 function App() {
 
   const [data, setData] = useState([]);
 
   useEffect(()=>{
-    axios.get('https://fakestoreapi.com/products').then(res=>{
-      console.log(res.data);
-      setData(res.data);
-    })
+    async function getData() {
+      await axios.get('https://fakestoreapi.com/products').then(res=>{
+        console.log(res.data);
+        setData(res.data);
+      })
+    }
+    getData()
+    
   },[])
   
   return (
@@ -27,26 +26,32 @@ function App() {
        <Grid container spacing={2}>
         {
           data.map((val, index)=>{
-            <Grid item xs={8}>
+            return(
+              <Grid item xs={8} md={4} lg={4} key={index}>
               <Card sx={{ maxWidth: 345 }}>
                 <CardMedia
                   component="img"
                   alt="img"
-                  height="140"
-                  image="/static/images/cards/contemplative-reptile.jpg"
+                  height="200"
+                  image = {val.image}
                 />
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="div">
-                    Lizard
+                    {val.title}
                   </Typography>
                   <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all continents except Antarctica
+                    {val.description}
+                  </Typography>
+                  <Typography variant="h6" sx={{ color: 'red' }}>
+                    {"$"+val.price}
                   </Typography>
                 </CardContent>
               </Card>
 
             </Grid>
+
+            )
+            
           })
         }
       </Grid>
